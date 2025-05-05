af9e631e4ef82f81a4d4418932417863d876eb0c
Roshaan Ali <roshaanalimeher@gmail.com>
2025-05-05 18:59:10 +0100

    update

---
+++ scrape-app/app/types.ts
@@ -0,0 +1,12 @@
+export type City = {
+    _id: string
+    postcode_area: string
+    area_covered: string
+    population_2011: number
+    households_2011: number
+    postcodes: number
+    active_postcodes: number
+    non_geographic_postcodes: number
+    scraped_at: string
+  }
+  
