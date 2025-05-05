3b6b1b0d720962b3bd2801da4acd27e5f8833221
Roshaan Ali <roshaanalimeher@gmail.com>
2025-05-05 16:16:05 +0100

    update

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import type React from "react"
+
 import { useState, useEffect, useRef, useCallback } from "react"
 import { useRouter } from "next/navigation"
 import styles from "../styles/HomePage.module.css"
